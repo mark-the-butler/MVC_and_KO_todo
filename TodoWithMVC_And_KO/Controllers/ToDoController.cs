1237cf5d3daa91e8a514881a0fadcdf328c843fb
mdb513 <mark.d.butler@sherwin.com>
2017-02-12 18:42:22 -0500

    Initial control with view

---
+++ TodoWithMVC_And_KO/Controllers/ToDoController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace TodoWithMVC_And_KO.Controllers
+{
+    public class ToDoController : Controller
+    {
+        // GET: ToDo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
