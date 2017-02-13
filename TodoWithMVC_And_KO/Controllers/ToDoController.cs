89c238cb6778cb653161c495b153bbed3d3cfa01
mdb513 <mark.d.butler@sherwin.com>
2017-02-12 19:50:22 -0500

    Added controller, thinking about workflow

--- TodoWithMVC_And_KO/Controllers/ToDoController.cs
+++ TodoWithMVC_And_KO/Controllers/ToDoController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using TodoWithMVC_And_KO.Models;
 
 namespace TodoWithMVC_And_KO.Controllers
 {
@@ -8,6 +10,16 @@ namespace TodoWithMVC_And_KO.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddTodo(string toDo)
+        {
+            var toDos = new ToDoViewModel();
+
+            var toDoList = toDos.ToDosList = new List<string>();
+            toDoList.Add(toDo);
+            return Redirect("./ToDo/Index");
         }
     }
 }
---
+++ TodoWithMVC_And_KO/Models/ToDoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TodoWithMVC_And_KO.Models
+{
+    public class ToDoViewModel
+    {
+        public List<string> ToDosList { get; set; }
+    }
+}
