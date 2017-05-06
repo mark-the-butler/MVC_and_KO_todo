045b9c81ed5ffb61b6e02d1a5541fa69537c0095
mysteryboy73 <mysteryboy7300@gmail.com>
2017-05-06 12:34:13 -0400

    Can get one item to the todo list

--- TodoWithMVC_And_KO/Controllers/ToDoController.cs
+++ TodoWithMVC_And_KO/Controllers/ToDoController.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Web.Mvc;
-using TodoWithMVC_And_KO.Models;
 
 namespace TodoWithMVC_And_KO.Controllers
 {
@@ -9,17 +8,14 @@ namespace TodoWithMVC_And_KO.Controllers
         // GET: ToDo
         public ActionResult Index()
         {
-            return View();
+            return View(new List<string>());
         }
 
         [HttpPost]
         public ActionResult AddTodo(string toDo)
         {
-            var toDos = new ToDoViewModel();
-
-            var toDoList = toDos.ToDosList = new List<string>();
-            toDoList.Add(toDo);
-            return Redirect("./ToDo/Index");
+            var toDoList = new List<string> {toDo};
+            return View("Index", toDoList);
         }
     }
 }
